04f02e4f6815161b429c3927274201b54bb59d79
John <johnnwaokolo81@gmail.com>
2025-02-14 22:07:00 +0100

    c

---
+++ Python/Conditional Statements and Date Time Module/Double_Check_Numbers.py
@@ -0,0 +1,12 @@
+num = int(input("Enter number to be checked: "))
+print("Number to be checked ", num)
+
+if num >= 50 :
+ if num%2== 0 :
+    print("This is an even number")
+ else :
+    print("This is an odd number")
+ print("This is greater than 50")
+
+else:
+    print("This is less than 50")
