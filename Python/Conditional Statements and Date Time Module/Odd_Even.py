cb474963168533a6c351e6d4eec16b10436613de
John <johnnwaokolo81@gmail.com>
2025-02-14 21:42:10 +0100

    c

---
+++ Python/Conditional Statements and Date Time Module/Odd_Even.py
@@ -0,0 +1,7 @@
+num = int(input("Enter number to be checked: "))
+print("Number to be checked ", num)
+
+if num%2== 0 :
+    print("This is an even number")
+else:
+    print("This is an odd number")
