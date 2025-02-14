dafa5af49d842da670619a04cd437736c974cf13
John <johnnwaokolo81@gmail.com>
2025-02-14 20:33:30 +0100

    commit

---
+++ Python/Operators and Data types/Python_Operators.py
@@ -0,0 +1,21 @@
+n1 = 45
+n2 = 3
+
+print("Number 1", n1)
+print("Number 2", n2)
+print("Addition", n1+n2)
+print("Subtraction", n1-n2)
+print("Product ", n1*n2)
+print("Division", n1/n2)
+print("Floor Division", n1//n2)
+print("Modulus Operation", n1%n2)
+print("Square", n2**2)
+print("Square root", n1**0.5)
+
+print("Equal?", n1==n2)
+print("Number 1 greater?", n1>n2)
+print("Number 2 greater", n1<n2)
+print("Not Equal?", n1!=n2)
+
+result = n1/n2+n2**2+10
+print("Result of given equation is:", result)
---
+++ Python/Operators and Data types/String_Operators.py
@@ -0,0 +1,15 @@
+first_name = "John"
+last_name = "Nwaokolo"
+full_name = first_name + last_name
+example= "ha"*5
+
+print("First Name", first_name)
+print("Last Name", last_name)
+print("Fulll Name", full_name)
+print("String multiplied by 5", example)
+
+word = "Coding"
+print("Length of string", len(word))
+print("First letter of string", word[0])
+print("Last letter of string", word[5])
+print("String Sliced", word[0:3])
