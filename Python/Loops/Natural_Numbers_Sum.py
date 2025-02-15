b5ab47956200065c069cb18cbecabcdeb1546513
John <johnnwaokolo81@gmail.com>
2025-02-15 22:57:47 +0100

    c

---
+++ Python/Loops/Natural_Numbers_Sum.py
@@ -0,0 +1,3 @@
+n = int(input("Enter the desired sum of natural numbers: "))
+total_sum = sum(range(1,n+1))
+print(f"The sum of the first {n} natural numbers is: {total_sum} ")
--- Python/Loops/Table_of_23.py
+++ Python/Loops/Table_of_23.py
@@ -1,2 +1,2 @@
 for i in range(0,11,2):
-    print(f"23 x {i} = {23*i}")
+    print(f"23 X {i} = {23*i}")
