d8e976a7e0833bc50b5a33e10534a041fdfa3526
John <johnnwaokolo81@gmail.com>
2025-02-16 21:58:06 +0100

    c

--- Python/Loops/Armstrong_Numbers.py
+++ Python/Loops/Armstrong_Numbers.py
@@ -2,3 +2,10 @@ n = int(input("Enter number to check: "))
  
 stg = str(n)
 num_digits = len(stg)
+
+armstrong_sum = sum(int(digit)**num_digits for digit in stg)
+
+if armstrong_sum == n :
+    print(f"{n} is an armstrong number")
+else:
+     print(f"{n} is not an armstrong number")
