52e8b988fec935ef37078c49f73da848cd7f88e4
John <johnnwaokolo81@gmail.com>
2025-02-16 21:49:35 +0100

    c

---
+++ Python/Functions/Introduce_Yourself.py
@@ -0,0 +1,6 @@
+def intro(name) :
+    print("Hello,\n Good evening, i am ", name) 
+
+user_name = input("Enter your name: ")
+intro(user_name)
+    
---
+++ Python/Loops/Armstrong_Numbers.py
@@ -0,0 +1,4 @@
+n = int(input("Enter number to check: "))
+ 
+stg = str(n)
+num_digits = len(stg)
