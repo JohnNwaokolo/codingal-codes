cce86d0bd3f3dcd6006269828012cd5b7195a285
John <johnnwaokolo81@gmail.com>
2025-12-25 02:19:56 +0100

    Tkinter Baby!!

--- Python/Denomination Calculator Using Tkinter/Rock_Paper_Scissors_GUI.py
+++ Python/Denomination Calculator Using Tkinter/Rock_Paper_Scissors_GUI.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import random as ran
 
+
 window = tk.Tk()
 window.title("Rock, Paper, Scissors")
 
@@ -8,58 +9,32 @@ window.title("Rock, Paper, Scissors")
 result_label = tk.Label(window, text="Make Your Choice!")
 result_label.pack(fill="x", padx= 50, pady=10)
 
-def play_rock():
+
+def play(user_choice):
+
     computer_choice = ran.choice(["Rock", "Paper", "Scissors"])
-    user_choice = "Rock"
 
-
-    if computer_choice == "Scissors":
-        result_label.config(text=f"Computer chose {computer_choice} | You chose {user_choice} | Rock smashes Scissors | Result: You win")
+    if user_choice == computer_choice:
+        result = "It's a Tie!"
     
-    elif computer_choice == "Rock":
-        result_label.config(text=f"Computer chose {computer_choice} | You chose {user_choice} | Rock and Rock | Result: Draw!")
+    elif (user_choice == "Rock" and computer_choice == "Scissors") or \
+         (user_choice == "Paper" and computer_choice == "Rock") or \
+         (user_choice == "Scissors" and computer_choice == "Paper"):
+        result = "You Win!"
 
     else:
-        result_label.config(text=f"Computer chose {computer_choice} | You chose {user_choice} | Paper covers Rock | Result: You lose!")
-       
+        result = "You Lose!"
 
-rock_button = tk.Button(window, text="Rock", command= play_rock)
+    result_label.config(text=f"Computer chose {computer_choice} | You chose {user_choice} | Result: {result}")
+    
+
+rock_button = tk.Button(window, text="Rock", command=lambda: play("Rock"))
 rock_button.pack(side="left", padx=20, pady=20)
 
-def play_paper():
-    computer_choice = ran.choice(["Rock", "Paper", "Scissors"])
-    user_choice = "Paper"
-
-    if computer_choice == "Rock":
-        result_label.config(text=f"Computer chose {computer_choice} | You chose {user_choice} | Paper covers Rock | Result: You win")
-
-    elif computer_choice == "Paper":
-        result_label.config(text=f"Computer chose {computer_choice} | You chose {user_choice} | Paper and Paper | Result: Draw!")
-
-    else:
-        result_label.config(text=f"Computer chose {computer_choice} | You chose {user_choice} | Scissors cuts Paper | Result: You lose!")
-
-
-
-paper_button = tk.Button(window, text="Paper", command= play_paper)
+paper_button = tk.Button(window, text="Paper", command=lambda: play("Paper"))
 paper_button.pack(side="top", pady=20)
 
-
-def play_scissors():
-    computer_choice = ran.choice(["Rock", "Paper", "Scissors"])
-    user_choice = "Scissors"
-
-    if computer_choice == "Paper":
-        result_label.config(text=f"Computer chose {computer_choice} | You chose {user_choice} | Scissors cuts Paper | Result: You win!")
-
-    elif computer_choice == "Scissors":
-        result_label.config(text=f"Computer chose {computer_choice} | You chose {user_choice} | Scissors and Scissors | Result: Draw!")
-
-    else:
-        result_label.config(text=f"Computer chose {computer_choice} | You chose {user_choice} | Rock smashes Scissors | Result: You lose!")
-
-
-scissors_button = tk.Button(window, text="Scissors", command=play_scissors)
+scissors_button = tk.Button(window, text="Scissors", command=lambda: play("Scissors"))
 scissors_button.pack(side="right", padx=20, pady=20)
 
 window.mainloop()
