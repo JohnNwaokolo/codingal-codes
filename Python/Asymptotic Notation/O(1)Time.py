c7e0b1c5dd0b475a8e9bb918e6f8261ef93f19ae
John <johnnwaokolo81@gmail.com>
2026-01-02 18:39:02 +0100

    tbju34

---
+++ Python/Asymptotic Notation/O(1)Time.py
@@ -0,0 +1,10 @@
+def printnumber(n):
+    literation = 0
+    literation += 1  # O(1) operation
+    print("The number entered is:", n)
+    print(f"Number of iterations:", literation)
+
+    print("With any 'n' value, the time taken remains constant.")
+
+printnumber(10)
+printnumber(200)
